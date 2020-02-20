a8590d6770349f77cb22f62ce5aa3d2cf0ca5064
Miles Higbie <miles.higbie@gmail.com>
2020-02-19 19:33:05 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     document.addEventListener("submit", function(event){
-//       event.preventDefault(); 
-//       const textBox = document.getElementById("new-task-description").value
-//       const li = document.createElement("li")
-//       const button = document.createElement("button")
-      
-//       button.innerText = "Delete"
-//       li.innerText = textBox
-      
-//       li.append(button)
-
-//       const ul = document.getElementById("tasks")
-
-//       ul.append(li)
-
-//       const deleteItem = function(){ 
-//         button.parentElement.remove()
-//       }
-
-//       button.addEventListener("click", deleteItem)
-//     })
-//   })
-
 document.addEventListener("DOMContentLoaded", () => {
 
   document.addEventListener("submit", function(event){
